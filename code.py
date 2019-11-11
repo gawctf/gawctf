1a8cc4793950c076f332425dae0f6ecfaf168dea
ZapFire <keke.schwarz999@gmail.com>
2019-11-11 18:47:17 -0500

    Deploying app
    Forgot I needed this shit to commit to master

---
+++ code.py
@@ -0,0 +1,16 @@
+import web
+
+from web.template import ALLOWED_AST_NODES
+ALLOWED_AST_NODES.append('Constant')
+
+urls = (
+'/input', 'index'
+)
+class index:
+    def GET(self):
+        i = web.input(name=None)
+        return render.index(i.name)
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+render = web.template.render('templates/')
