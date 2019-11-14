e1f84b652f1ded6f10ce6c646cdbc55e4162f713
ZapFire <keke.schwarz999@gmail.com>
2019-11-14 14:12:57 -0500

    SNENTRY plS

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ from flask import Flask
 import sentry_sdk
 
 sentry_sdk.init(release="gawctf@v1")
+sentry_sdk.init(dsn='https://c4c9eb5cffc34cb191b534685d07b669@sentry.io/1820760')
 
 from web.template import ALLOWED_AST_NODES
 ALLOWED_AST_NODES.append('Constant')
